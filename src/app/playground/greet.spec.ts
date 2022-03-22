b63713d330fca394ab0cabb446776392a584503d
meluferiev <meluferiev@axbit.ru>
2022-03-22 12:05:30 +0400

    init: start

---
+++ src/app/playground/greet.spec.ts
@@ -0,0 +1,10 @@
+import { greet } from "./greet"
+
+
+describe('greet', () => {
+  
+  it('should include name in return message', () => {
+    expect(greet('Angular')).toContain('Angular');
+  })
+
+})
